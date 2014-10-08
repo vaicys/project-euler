eaeb7893c5dca52bfd06e0b9e3c2aa0e88be3f1c
Vytautas Vaičys <vytautas@vaicys.com>
2014-10-08 22:05:45 +0300

    Final solution for 64.

--- problem64-v2.py
+++ problem64-v2.py
@@ -2,7 +2,10 @@ from math import sqrt
 
 
 def periodic_root(number):
+    if int(sqrt(number))**2 == number:
+        return None
     seq = []
+    fraq_seq = []
     a = int(sqrt(number))
     initial = a
     d = -a
@@ -14,117 +17,37 @@ def periodic_root(number):
     while n - d >= -initial:
         a += 1
         n -= d
-    print("a = ", a, "frac = ", n, "/", d)
+    seq.append(a)
+    fraq_seq.append((n, d))
 
-    oldn = n
-    (n, d) = (d, n)
+    while True:
 
-    d = number - abs(d)**2
-    if n > 1:
-        d = int(d / n)
-    n = -oldn
+        oldn = n
+        (n, d) = (d, n)
 
-    a = 0
-    while n - d > -initial:
-        a += 1
-        n -= d
-    print("a = ", a, "frac = ", n, "/", d)
+        d = number - abs(d)**2
+        if n > 1:
+            d = int(d / n)
+        n = -oldn
 
-    oldn = n
-    (n, d) = (d, n)
-
-    d = number - abs(d)**2
-    if n > 1:
-        d = int(d / n)
-    n = -oldn
-
-    a = 0
-    while n - d >= -initial:
-        a += 1
-        n -= d
-    print("a = ", a, "frac = ", n, "/", d)
+        a = 0
+        while n - d >= -initial:
+            a += 1
+            n -= d
+        seq.append(a)
+        fraq = (n, d)
+        if fraq in fraq_seq:
+            i = fraq_seq.index(fraq)
+            return seq[i:-1]
+        fraq_seq.append(fraq)
 
 
+count = 0
+for i in range(1, 10000 + 1):
+    result = periodic_root(i)
+    if result is not None:
+        #print(i, result)
+        if len(result) % 2 != 0:
+            count += 1
 
-    oldn = n
-    (n, d) = (d, n)
-
-    d = number - abs(d)**2
-    if n > 1:
-        d = int(d / n)
-    n = -oldn
-
-    a = 0
-    while n - d >= -initial:
-        a += 1
-        n -= d
-    print("a = ", a, "frac = ", n, "/", d)
-
-
-    oldn = n
-    (n, d) = (d, n)
-
-    d = number - abs(d)**2
-    if n > 1:
-        d = int(d / n)
-    n = -oldn
-
-    a = 0
-    while n - d >= -initial:
-        a += 1
-        n -= d
-    print("a = ", a, "frac = ", n, "/", d)
-
-
-
-
-    oldn = n
-    (n, d) = (d, n)
-
-    d = number - abs(d)**2
-    if n > 1:
-        d = int(d / n)
-    n = -oldn
-
-    a = 0
-    while n - d >= -initial:
-        a += 1
-        n -= d
-    print("a = ", a, "frac = ", n, "/", d)
-
-
-
-
-    oldn = n
-    (n, d) = (d, n)
-
-    d = number - abs(d)**2
-    if n > 1:
-        d = int(d / n)
-    n = -oldn
-
-    a = 0
-    while n - d >= -initial:
-        a += 1
-        n -= d
-    print("a = ", a, "frac = ", n, "/", d)
-
-
-
-    oldn = n
-    (n, d) = (d, n)
-
-    d = number - abs(d)**2
-    if n > 1:
-        d = int(d / n)
-    n = -oldn
-
-    a = 0
-    while n - d >= -initial:
-        a += 1
-        n -= d
-    print("a = ", a, "frac = ", n, "/", d)
-
-
-
-print(periodic_root(2))
+print(count)
